1925a7e6988b62feb1e35b1ee378d4b329968429
Thorsten Weber <tweberfl@gmail.com>
2018-01-31 08:52:23 +0100

    Use withMVar

--- src/Data/Storage/User.hs
+++ src/Data/Storage/User.hs
@@ -24,18 +24,14 @@ instance FromRow User where
 fetchUser :: MVar Connection
           -> String
           -> IO (Maybe User)
-fetchUser mVarConn username = do
-  conn <- takeMVar mVarConn
-  mUser <- queryDb conn username
-  putMVar mVarConn conn
-  pure mUser
+fetchUser mVarConn username = withMVar mVarConn $ queryDb `flip` username
   where
-    queryDb conn username = do
-      users <-  query conn "SELECT id, username, public_key, debts from user WHERE username = ?" [username] :: IO [User]
+    queryDb conn' username' = do
+      users <-  query conn' "SELECT id, username, public_key, debts from user WHERE username = ?" [username'] :: IO [User]
       case users of
         []      -> pure Nothing
         [!user] -> pure $ Just user
         (_:_)   -> pure Nothing -- Should never happen, since the username is a primary key
 
 incDebts :: MVar Connection -> String -> Float -> IO ()
-incDebts mVarConn username factor = withMVar mVarConn $ \conn -> execute conn "UPDATE user SET debts = debts + ? WHERE username = ?" (factor, username)
+incDebts mVarConn username summand = withMVar mVarConn $ \conn -> execute conn "UPDATE user SET debts = debts + ? WHERE username = ?" (summand, username)
