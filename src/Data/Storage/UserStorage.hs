6ea90aff9c485cd2f95bf03feeb7c0b9fe801f7c
Thorsten Weber <tweberfl@gmail.com>
2018-01-31 07:20:56 +0100

    Add depts functions

--- src/Data/Storage/UserStorage.hs
+++ src/Data/Storage/UserStorage.hs
@@ -9,59 +9,47 @@ module Data.Storage.UserStorage (
 import           Control.Concurrent.MVar
 import           Database.SQLite.Simple
 
-data UserStorage = UserStorage { _conn  :: MVar Connection
-                               , _fetch :: String -> IO (Maybe User)
+data UserStorage = UserStorage { _conn      :: MVar Connection
+                               , _fetchUser :: String -> IO (Maybe User)
+                               , _incDepts  :: String -> Float -> IO ()
+                               , _decDepts  :: String -> Float -> IO ()
 }
 
-newUserStorage :: IO UserStorage
-newUserStorage = do
-  conn <- open "test.db"
-  initialize conn
-  mVarConn <- newMVar conn
+newUserStorage :: MVar Connection -> IO UserStorage
+newUserStorage mVarConn = do
+  initialize mVarConn
   pure $ UserStorage {
     _conn = mVarConn
-  , _fetch = fetch' mVarConn
+  , _fetchUser = fetchUser mVarConn
+  , _incDepts = incDepts mVarConn
+  , _decDepts = decDepts mVarConn
   }
 
-data User = User { _id        :: Int
+data User = User { _id        :: !Int
                  , _username  :: String
                  , _publicKey :: String
-                 , _depts     :: Float
+                 , _depts     :: !Float
 } deriving (Show)
 
 instance FromRow User where
   fromRow = User <$> field <*> field <*> field <*> field
 
-initialize :: Connection -> IO ()
-initialize conn = execute_ conn "CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY, username TEXT unique, public_key TEXT, depts FLOAT)"
+initialize :: MVar Connection -> IO ()
+initialize mVarConn = withMVar mVarConn $ execute_ `flip` "CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY, username TEXT unique, public_key TEXT, depts REAL)"
 
-add :: Connection -> String -> String -> IO ()
-add _ "" _ = pure ()
-add _ _ "" = pure ()
-add conn username publicKey = execute conn "INSERT INTO user (username, public_key, depts) VALUES (?, ?, ?)" (username, publicKey, 0 :: Float)
+fetchUser :: MVar Connection -> String -> IO (Maybe User)
+fetchUser mVarConn username = withMVar mVarConn $ fetchUser' `flip` username
+  where
+    fetchUser' conn username = do
+      users <- query conn "SELECT id, username, public_key, depts from user WHERE username = ?" [username] :: IO [User]
+      case users of
+        []       -> pure Nothing
+        [user]   -> pure $ Just user
+        (user:_) -> pure Nothing -- Should never happen
 
-remove :: Connection -> String -> IO ()
-remove conn username = execute conn "DELETE FROM user WHERE username = ?" (Only username)
+incDepts :: MVar Connection -> String -> Float -> IO ()
+incDepts mVarConn username factor = withMVar mVarConn $ \conn -> execute conn "UPDATE user SET depts = depts + ? WHERE username = ?" (factor, username)
 
-fetch :: Connection -> String -> IO (Maybe User)
-fetch _ "" = pure Nothing
-fetch conn username = do
-  users <-  query conn "SELECT id, username, public_key, depts from user WHERE username = ?" [username] :: IO [User]
-  case users of
-    []       -> pure Nothing
-    [user]   -> pure $ Just user
-    (user:_) -> pure Nothing -- Should never happen
-
-fetch' :: MVar Connection -> String -> IO (Maybe User)
-fetch' mVarConn username = do
-  conn <- takeMVar mVarConn
-  mUser <- fetch conn username
-  putMVar mVarConn conn
-  pure mUser
-
-
-showUser :: Maybe User -> IO ()
-showUser mUser = case mUser of
-  Nothing   -> print "Gibbet net"
-  Just user -> print $ _username user
+decDepts :: MVar Connection -> String -> Float -> IO ()
+decDepts mVarConn username factor = withMVar mVarConn $ \conn -> execute conn "UPDATE user SET depts = depts - ? WHERE username = ?" (factor, username)
 
