6a4326ea31f4644f7a4473253e0d32784fd06406
Sebastian Clausen <github@sebastian-clausen.de>
2018-02-26 11:30:15 +0100

    Autogenerate banner from version.

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import           AnsiText
 import           Data.Storage.Storage
+import           Data.Version
+import           Paths_haskell_bas        (version)
 import           Repl
 import           System.Console.Haskeline
 
 main :: IO ()
 main = do
+  putStrLn $ makeAnsiText $ "bas " ++ showVersion version
   storage <- newStorage
   settings <- makeSettings
-  putStrLn ""
-  putStrLn " ██████╗  █████╗ ███████╗     ██╗    ██╗"
-  putStrLn " ██╔══██╗██╔══██╗██╔════╝    ███║   ███║"
-  putStrLn " ██████╔╝███████║███████╗    ╚██║   ╚██║"
-  putStrLn " ██╔══██╗██╔══██║╚════██║     ██║    ██║"
-  putStrLn " ██████╔╝██║  ██║███████║     ██║██╗ ██║"
-  putStrLn " ╚═════╝ ╚═╝  ╚═╝╚══════╝     ╚═╝╚═╝ ╚═╝"
-  putStrLn ""
   runInputT settings (repl storage)
